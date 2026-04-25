32b89d87a3962da3071f5c3b49d9a0c9f1edf471
giulio37200 <giulio.mch@gmail.com>
2026-04-25 11:22:53 +0200

    Configure PilotPay with Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PILOTPAY_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://tybhaxyqmopomkcqvsks.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5YmhheHlxbW9wb21rY3F2c2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwODcwNDYsImV4cCI6MjA5MjY2MzA0Nn0.AMbrwWj1WE4w1tWDm6bk_E2G_pLYF_wlwfa2TGa6vAk",
   resetRedirectTo: window.location.origin + window.location.pathname,
 };
